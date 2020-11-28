3db6b320a924995c20a9097cf81e95c9c9d3242a
Isabeau Rea <isabeaurea@gmail.com>
2020-11-28 15:02:03 -0500

    d

--- public/script.js
+++ public/script.js
@@ -37,3 +37,26 @@ const searchInput = document.querySelector('.search');
 const suggestions = document.querySelector('.suggestions');
 
 searchInput.addEventListener('input', displayMatches);
+
+
+
+
+const search = document.querySelector('.dropdown');
+
+search.addEventListener('click', async (e) => {
+  e.preventDefault();
+  const form = $(e.target).serializeArray();
+  fetch('/api', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(form)
+  })
+    .then((fromServer) => fromServer.json())
+    .then((fromServer) => {
+    
+      console.log(fromServer)
+})
+.catch((err) => console.log(err));
+});
