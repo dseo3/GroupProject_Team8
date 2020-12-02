747d8248cbd7f70caba58f35918a95d70993dc22
a17omogb <57695301+a17omogb@users.noreply.github.com>
2020-12-02 14:55:57 -0500

    moves js to script.js

--- public/script.js
+++ public/script.js
@@ -92,7 +92,7 @@ searchInput.addEventListener('input', displayMatches);
 */
 
 
-
+/*
 //preferences dropdown bar
 
 let dropdown = $('#grad-program');
@@ -110,22 +110,42 @@ $.getJSON(url, function (data) {
     dropdown.append($('<option></option>').attr('value', courses.dept_id).text(courses.department));
   })
 });
+*/
+
+//PREFERENCES JAVASCRIPT
+
+const dep_api_url = 'https://api.umd.io/v0/courses/departments?semester=202008' ;
 
 
-//fetching for department data
-const proxyurl = "https://cors-anywhere.herokuapp.com/";
-const dep_api_url = 'https://api.planetterp.com/v1/courses?department=MATH&limit=1' ;
+   window.onload = async function getDepartments() {
+     //get department data from api
+     const response = await fetch(dep_api_url);
+     const json = await response.json();
+     
+     console.log(json);
+    
+     //create a list of departments
+                var departments = json
+                var dep_list = []
+            for (var i = 0; i < departments.length; i++) {
+                dep_list.push(departments[i].department);
+            }
+      
+      //add department list to drop down menu
+            var select = document.getElementById("grad-program");
+            for(var i in dep_list){
+              var option = document.createElement('option');
+              option.text = option.value = dep_list[i];
+              select.add(option, 0);
 
-              fetch('https://api.planetterp.com/v1/courses?department=MATH&limit=1',
-        {
-          method: 'GET'
+            }
+            
+            
+            console.log(dep_list);
+            //document.getElementById('grad-program').innerHTML = dep_list ;
+   }
+   //getDepartments();
 
-        })
-        .then(function(res) {
-            return res.json();
-        }).then(function(body) {
-            console.log(body);
-        });
 
 
 /*
