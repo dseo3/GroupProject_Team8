f27cd5cd7f6adf87a6a850a2a6a1aa4493672479
AaronA35 <aarchamp@terpmail.umd.edu>
2020-12-06 16:42:24 -0500

    save

--- public/app.js
+++ public/app.js
@@ -17,6 +17,56 @@ ul.innerHTML = li
 
 target.appendChild(ul)
     })
+    
+     // Get the modal
+     var modal = document.getElementById("myModal");
+
+     // Get the button that opens the modal
+     var btn = document.getElementById("myBtn");
+
+     // Get the <span> element that closes the modal
+     var span = document.getElementsByClassName("close")[0];
+
+     // When the user clicks the button, open the modal 
+     btn.onclick = function() {
+     modal.style.display = "block";
+     }
+
+     // When the user clicks on <span> (x), close the modal
+     span.onclick = function() {
+     modal.style.display = "none";
+     }
+
+     const umd_courses= "https://api.umd.io/v0/courses/list?semester=202008";
+
+     fetch(umd_courses) 
+         .then((response) => response.json())
+         .then((data) => {
+
+             var select = document.getElementById("course");
+             for (var i in data) {
+                 var option1 = document.createElement("option");
+                 option1.text = option1.value = data[i].course_id;
+                 select.add(option1);
+             }
+
+     });
+     
+     const umd_io = "https://api.umd.io/v1/courses/sections?semester=202008"
+     
+     fetch(umd_io) 
+         .then((response) => response.json())
+         .then((data) => {
+
+             var select = document.getElementById("section");
+             for (var i in data[0]) {
+                 var option = document.createElement("option");
+                 option.text = option.value = data[0][i].section_id;
+                 select.add(option);
+             }
+
+
+     });
 //}
 
 
