40d5a9d9c5f445a4469f87528da6c5dd28539a6b
AaronA35 <aarchamp@terpmail.umd.edu>
2020-12-06 15:57:20 -0500

    search page updates

--- public/app.js
+++ public/app.js
@@ -4,17 +4,20 @@
     .then((response) => {return response.json()})
     .then((response) => {
         console.log(response[1].dept_id)
-        const target = document.querySelector(".target")
+        const target = document.querySelector(".search")
 const ul = document.createElement('ul')
 let li = response.map((dept)=> `<li>
-<span class="dept_id">${dept.dept_id}</span><br>
-<span class="category">${dept.department}</span>
-</li>`)
-    
+                <div class="tile is-parent" >
+                    <div class="tile is-child box" id="department-name">
+                        <p class="title"> <b>${dept.dept_id}</b> <small>${dept.department}</small></p>
+                    </div>
+                </div>
+            </li>`)
+
 ul.innerHTML = li
-const Element = document.querySelector('.Element');
 
 target.appendChild(ul)
     })
 //}
+
 
