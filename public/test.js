ff7becdecff4413367a25346ce4d44b757145ebc
Isabeau Rea <isabeaurea@gmail.com>
2020-12-08 18:58:29 -0500

    ALEX CHANGES AS WELL

--- public/test.js
+++ public/test.js
@@ -52,7 +52,12 @@ function displayMatches() {
   return HTMLmatches;
 }
 
-// Lil Yomi's Code
+
+
+
+
+
+// Yomi's Code
 
 const dep_api_url = "https://api.umd.io/v1/courses/departments?semester=202008";
 
@@ -81,6 +86,62 @@ async function getDepartments() {
   console.log(dep_list);
   //document.getElementById('grad-program').innerHTML = dep_list ;
 }
+
+
+
+
+
+//Fetching PlanetTerp API
+const proxyurl = "https://cors-anywhere.herokuapp.com/";
+const urlTerp = "https://api.planetterp.com/v1/grades?course=INST612"; // site that doesn’t send Access-Control-*
+let TotalClassGPA = 0.0;
+
+fetch(proxyurl + urlTerp) // https://cors-anywhere.herokuapp.com/https://example.com
+
+
+  .then((response) => response.json())
+  .then((data) => {
+    availCourses.forEach((item) => { //don't remember what availCourses is supposed to be, but alex added it, so I need to go back and figure it out again. - Isabeau
+      let total =
+        item["A+"] * 4.0 +
+        item["A"] * 4.0 +
+        item["A-"] * 3.7 +
+        item["B+"] * 3.3 +
+        item["B"] * 3.0 +
+        item["B-"] * 2.7 +
+        item["C+"] * 2.3 +
+        item["C"] * 2.0 +
+        item["C-"] * 1.7 +
+        item["D+"] * 1.3 +
+        item["D"] * 1.0 +
+        item["D-"] * 0.7;
+
+      let numStudents =
+        item["A+"] +
+        item["A"] +
+        item["A-"] +
+        item["B+"] +
+        item["B"] +
+        item["B-"] +
+        item["C+"] +
+        item["C"] +
+        item["C-"] +
+        item["D+"] +
+        item["D"] +
+        item["D-"] +
+        item["F"];
+
+      TotalClassGPA += total / numStudents;
+    });
+
+    TotalClassGPA /= data.length;
+    console.log(TotalClassGPA.toFixed(2));
+    document.getElementById("avgGrade").innerHTML =
+      "<b>" + "Average Grade: " + "</b>" + TotalClassGPA.toFixed(2);
+  });
+
+
+
 
 
 
