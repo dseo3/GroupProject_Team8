5ce5b85f15b49d6a0fb475a9cbdafbbe0a91f8d9
kswhite15 <58196468+kswhite15@users.noreply.github.com>
2020-12-11 02:23:45 -0500

    api url & removing courses

--- public/test.js
+++ public/test.js
@@ -6,15 +6,39 @@ async function main() {
 
   // STUFF ISABEAU ADDED FOR STRING FORMATTING THE URL
   const dept_id_here = document.forms[0].elements[0];
-  const dept_id_for_data = dept_id_here.value[0]+dept_id_here.value[1]+dept_id_here.value[2]+dept_id_here.value[3];
+  //No longer Spaghetti code yay!
+  const dept_id_for_data = dept_id_here.value.substring(0,4);
+  console.log("Selected Department Code:", dept_id_for_data);
+  // Just stringing together the API url here before we fetch the data 
+  const pref_api = "https://api.umd.io/v1/courses?dept_id=" + dept_id_for_data
 
-  // const data = await fetch("https://api.umd.io/v1/courses");
-  const data = await fetch(
-    "https://api.umd.io/v1/courses?dept_id=" + dept_id_for_data
-  ); // PREVIOUSLY"https://api.umd.io/v1/courses?semester=202008");
+  /*The console.log below shows that it actually fetching the API data 
+    if you click the link in the console after inspecting the page
+    it should take you too the application tab and you can see all the courses
+    for the selected department, so Good Job ISABEAU!
+    Hoowwwever something weird is happening here when we try and fetch that data
+  
+  */
+  console.log("API url is", pref_api)
+
+  /* 
+  I've tried putting the first paginated API back in 
+  and I'm getting the same weird response when I fetch the data
+  so I do not the the pref_api variable is wrong it is something else 
+  that is refusing to to pull the data from the link
+
+  I think we need to get just the const data, from below 
+  working again and then replace it with pref_api
+  */
+
+   //const data = await fetch("https://api.umd.io/v1/courses");
+  
+  const data = await fetch(pref_api); 
+  // PREVIOUSLY"https://api.umd.io/v1/courses?semester=202008");
+    
+  console.log(data, "THIS IS WHERE THE MATCH HAPPENS")
+  
   //parses api data into json value
-    console.log(data, "THIS IS WHERE THE MATCH HAPPENS")
-
   const courses = await data.json(); 
   const searchInput = document.querySelector(".search");  //TBD not being used right now -> will be used for serach page 
   const suggestions = document.querySelector(".suggestions"); //not being used right now
@@ -127,7 +151,6 @@ function avgGPA(course_id) {
   });
 }
 
-
 //Show New Course Recommendation and Save To Bookmarks
 function NewRecFromFave(availCourses){
   const favbutton = document.querySelector("#fav_button");
@@ -166,7 +189,6 @@ function NewRecFromX(availCourses){
   })
 }
 
-
 function findMatches(wordsToMatch, courses) {
   return courses.filter((course) => {
     const regex = new RegExp(wordsToMatch, "gi");
@@ -191,8 +213,6 @@ function displayMatches() {
 
   return HTMLmatches;
 }
-
-
 
 // Yomi's Code: for Preferences Departments Drop down at top of index/home page
 const dep_api_url = "https://api.umd.io/v1/courses/departments?semester=202101"; // I ALSO CHANGED THE SEMESTER HERE - ISABEAU
@@ -224,5 +244,12 @@ async function getDepartments() {
   //document.getElementById('grad-program').innerHTML = dep_list ;
 }
 
+//Removing saved course when you click the bookmark button
+function removeSavedCourse() {
+  console.log("removing this course")
+  const savedcourse = document.getElementById("saved_couse");
+  savedcourse.remove();
+}
+removeSavedCourse;
 
 window.onload = main;
