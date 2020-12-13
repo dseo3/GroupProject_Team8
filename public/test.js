e76885d5b6d93f2cc420cd353e3ba80c4240f0cf
Isabeau Rea <isabeaurea@gmail.com>
2020-12-13 17:22:08 -0500

    Fixed 3-page setup

--- public/test.js
+++ public/test.js
@@ -314,9 +314,10 @@ function removeSavedCourse() {
 
 
 // REPLACE THIS WITH QUERY SELECTOR
-function show(shown, hidden) {
+function show(shown, hidden1, hidden2) {
   document.getElementById(shown).style.display='block';
-  document.getElementById(hidden).style.display='none';
+  document.getElementById(hidden1).style.display='none';
+  document.getElementById(hidden2).style.display='none';
   return false;
 };
 
