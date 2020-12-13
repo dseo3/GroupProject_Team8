b7d0f4f40774c5720b3f86e15bbf1e5f10e7942e
Isabeau Rea <isabeaurea@gmail.com>
2020-12-13 17:50:58 -0500

    Revert "Merge branch 'main' of https://github.com/dseo3/GroupProject_Team8 into main"
    This reverts commit 717a4cd7139a9c7170b8d2b34c1204850250b97d, reversing
    changes made to 0f19f7606a2f0f986e603cb5f14f956e311b4c0e.

--- public/test.js
+++ public/test.js
@@ -266,7 +266,7 @@ function NewRecFromFave(courses, random){
             </div>
           </div>
           <div class="learn-more-button">  
-          <a href="#" onclick="return show('details-page','index_page','bookmarks_page');" class="learn">
+          <a href="course.html" class="learn">
             <button class="learn-more">Learn More</button>
           </a> 
         </div>            
@@ -363,10 +363,9 @@ function removeSavedCourse() {
 
 
 // REPLACE THIS WITH QUERY SELECTOR
-function show(shown, hidden1, hidden2) {
+function show(shown, hidden) {
   document.getElementById(shown).style.display='block';
-  document.getElementById(hidden1).style.display='none';
-  document.getElementById(hidden2).style.display='none';
+  document.getElementById(hidden).style.display='none';
   return false;
 };
 
