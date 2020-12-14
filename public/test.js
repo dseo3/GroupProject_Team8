9d1019df1d5ed6114c89aa630b7add12ac82b7f9
kswhite15 <58196468+kswhite15@users.noreply.github.com>
2020-12-14 14:52:55 -0500

    new style for bookmarks

--- public/test.js
+++ public/test.js
@@ -198,39 +198,21 @@ function NewRecFromFave(){
       <li id=${currCourse.course_id}>
         <div class="tile is-parent tester_item">
           <div class="tile is-child box" id="saved-course">
-            <div id="course-info">      
-            
+            <div id="course-info">   
               <p class="title" id="bookmark_item"> <b>${currCourse.course_id}</b> <small>${currCourse.name}</small></p>
-            
               <button class="bookmark_button" onclick="removeSavedCourse(${currCourse.course_id})"> <i class="fas fa-bookmark fa-2x"></i> </button>
             </div>
-              <div class="course-stats">
-              <div class="tile is-ancestor">
-                <div class="tile is-parent">
-                  <article class="tile is-child box" id="course-stat">
-                    <p class="title" id="credit">${currCourse.credits}</p>
-                    <p class="subtitle">Credits</p>
-                  </article>
-                </div>
-                <div class="tile is-parent">
-                  <article class="tile is-child box" id="course-stat">
-                    <p class="title" id="gened">${currCourse.gen_ed}</p>
-                    <p class="subtitle">Gen-Ed</p>
-                  </article>
-                </div>
-                <div class="tile is-parent">
-                  <article class="tile is-child box" id="course-stat">
-                    <p class="title" id="method">${currCourse.grading_method}</p>
-                    <p class="subtitle">Grading Method</p>
-                  </article>
-                </div>
-              </div>
+            <div class="tile is-parent">
+              <article class="tile is-child box" id="course-stat">
+                <p class="title">Description</p>  
+                <p class="subtitle" id="book_descrip">${currCourse.description}</p>
+              </article>
             </div>
             <div class="learn-more-button ${currCourse.course_id}">  
-            <a href="#" class="learn" onclick="DetailsPage(${currCourse.course_id}, book_collection);return showpage('details-page','index_page','bookmarks_page');">
-              <button class="learn-more">Learn More</button>
-            </a> 
-          </div>            
+              <a href="#" class="learn" onclick="DetailsPage(${currCourse.course_id}, book_collection);return showpage('details-page','index_page','bookmarks_page');">
+                <button class="learn-more">Learn More</button>
+              </a> 
+            </div>            
           </div>
         </div>
       </li>`;
